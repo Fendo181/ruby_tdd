701541c375fc0d76178f840e855f0a6cb68cb5bd
endu <futoshi.endo@pepabo.com>
2017-12-19 17:36:44 +0900

    fake it

--- spec/file/fibonacci_spec.rb
+++ spec/file/fibonacci_spec.rb
@@ -12,17 +12,21 @@ def fibonacci_calc(n)
 end
 
 describe 'Fibonacci' do
-  arr = [[0,0],[1,1],[2,1]]
+  arr = [[0,0],[1,1],[2,1],[3,2]]
 
-  it '0が返ってくる' do
+  it 'a1=0の時、a2=0になる' do
     expect(arr[0][1]).to eq (fibonacci_calc(arr[0][0]))
   end
 
-  it '1が返ってくる' do
+  it 'a1=1時、a2=1になる' do
     expect(arr[1][1]).to eq (fibonacci_calc(arr[1][0]))
   end
 
-  it '2が返ってくる' do
+  it 'a1=2時、a2=2になる' do
     expect(arr[2][1]).to eq (fibonacci_calc(arr[2][0]))
+  end
+
+  it 'a1=3時、a2=2になる' do
+    expect(arr[3][1]).to eq (fibonacci_calc(arr[3][0]))
   end
 end
