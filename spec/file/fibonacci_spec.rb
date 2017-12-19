cc8864279edbd87fae61c02a17fcfca533c629f5
endu <futoshi.endo@pepabo.com>
2017-12-19 16:14:22 +0900

    まずは落ちるテストを実装した

--- spec/file/fibonacci_spec.rb
+++ spec/file/fibonacci_spec.rb
@@ -1,2 +1,10 @@
 require "spec_helper"
 require File.expand_path('../tdd_ruby')+"/Fibonacci"
+
+describe 'Fibonacci' do
+  context '0が入る時' do
+    it '0が返ってくる' do
+      expect(fibonacci_calc(0)).to eq (0)
+    end
+  end
+end
