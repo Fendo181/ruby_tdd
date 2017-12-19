120b330811fef6e49aa862150c24260a1f36f37a
endu <futoshi.endo@pepabo.com>
2017-12-19 17:30:10 +0900

    テストを修正

--- spec/file/fibonacci_spec.rb
+++ spec/file/fibonacci_spec.rb
@@ -4,17 +4,25 @@ require "spec_helper"
 def fibonacci_calc(n)
   if (n==0)
     return 0
-  else
+  elsif (n <= 2)
     return 1
+  else
+    return 2
   end
 end
 
 describe 'Fibonacci' do
+  arr = [[0,0],[1,1],[2,1]]
+
   it '0が返ってくる' do
-    expect(fibonacci_calc(0)).to eq (0)
+    expect(arr[0][1]).to eq (fibonacci_calc(arr[0][0]))
   end
 
   it '1が返ってくる' do
-    expect(fibonacci_calc(1)).to eq (1)
+    expect(arr[1][1]).to eq (fibonacci_calc(arr[1][0]))
+  end
+
+  it '2が返ってくる' do
+    expect(arr[2][1]).to eq (fibonacci_calc(arr[2][0]))
   end
 end
