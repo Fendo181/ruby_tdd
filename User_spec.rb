25d791cbfce0b28dfbfafa990331145b1b442d3d
endu <futoshi.endo@pepabo.com>
2017-12-19 15:39:17 +0900

    setterのテストを追加した

--- User_spec.rb
+++ User_spec.rb
@@ -4,16 +4,21 @@ require File.dirname(__FILE__) + "/User"
 
 describe 'User' do
 
-  it '.initialize name' do
+  it '.initialize getter' do
     endo = User.new("endo")
     expect(endo.name).to eq("endo")
+  end
+
+  it '.initialize setter' do
+    endo = User.new("endo")
+    endo.name  =  "Takahashi"
+    expect(endo.name).to eq("Takahashi")
   end
 
   it '.sayHi' do
     endo = User.new("endo")
     expect(endo.name).to eq("endo")
   end
-
 end
 
 
