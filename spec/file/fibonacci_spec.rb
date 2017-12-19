9e18a3d8fc431cfaa41bbf6ee8e9cb4e4cc2ffad
endu <futoshi.endo@pepabo.com>
2017-12-19 17:40:06 +0900

    文言を修正した

--- spec/file/fibonacci_spec.rb
+++ spec/file/fibonacci_spec.rb
@@ -14,19 +14,19 @@ end
 describe 'Fibonacci' do
   arr = [[0,0],[1,1],[2,1],[3,2]]
 
-  it 'a1=0の時、a2=0になる' do
+  it 'nが0' do
     expect(arr[0][1]).to eq (fibonacci_calc(arr[0][0]))
   end
 
-  it 'a1=1時、a2=1になる' do
+  it 'nが1' do
     expect(arr[1][1]).to eq (fibonacci_calc(arr[1][0]))
   end
 
-  it 'a1=2時、a2=2になる' do
+  it 'nが2' do
     expect(arr[2][1]).to eq (fibonacci_calc(arr[2][0]))
   end
 
-  it 'a1=3時、a2=2になる' do
+  it 'nが3' do
     expect(arr[3][1]).to eq (fibonacci_calc(arr[3][0]))
   end
 end
