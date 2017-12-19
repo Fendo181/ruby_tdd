b907b84e4b7506e37e12739d35f32522ced85b7b
endu <futoshi.endo@pepabo.com>
2017-12-19 16:17:43 +0900

    Fake it (仮実装)

--- spec/file/fibonacci_spec.rb
+++ spec/file/fibonacci_spec.rb
@@ -1,10 +1,12 @@
 require "spec_helper"
-require File.expand_path('../tdd_ruby')+"/Fibonacci"
+# require File.expand_path('../tdd_ruby')+"/Fibonacci"
+
+def fibonacci_calc(num)
+  return 0
+end
 
 describe 'Fibonacci' do
-  context '0が入る時' do
-    it '0が返ってくる' do
-      expect(fibonacci_calc(0)).to eq (0)
-    end
+  it '0が返ってくる' do
+    expect(fibonacci_calc(0)).to eq (0)
   end
 end
